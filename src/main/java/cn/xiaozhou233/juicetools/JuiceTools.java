bd70c3de9e05c1f732909352df266b3b382bdf83
筱周XiaoZhou <xiaozhou233@vip.qq.com>
2025-10-20 15:29:55 +0800

    [Update] Basic GUI

--- src/main/java/cn/xiaozhou233/juicetools/JuiceTools.java
+++ src/main/java/cn/xiaozhou233/juicetools/JuiceTools.java
@@ -1,15 +1,51 @@
 package cn.xiaozhou233.juicetools;
 
+import cn.xiaozhou233.juiceloader.JuiceLoaderNative;
+import cn.xiaozhou233.juicetools.gui.StartGui;
 import cn.xiaozhou233.juicetools.network.HttpServer;
 
 import java.io.IOException;
 
 public class JuiceTools {
+    private static JuiceLoaderNative loaderNative;
     public static void main(String[] args) {
+        System.out.println("JuiceTools is starting...");
+        init();
+    }
+
+    public static void init() {
+        System.out.println("Starting GUI...");
+        StartGui.start();
+        StartGui.showInfo("Init!");
+
+        System.out.println("Loading Library...");
+        StartGui.showInfo("Loading Library...");
         try {
-            new HttpServer(8080);
-        } catch (IOException e) {
+            System.load(ClassLoader.getSystemResource("lib/libjuiceloader.dll").getPath());
+        } catch (Exception e) {
+            StartGui.showInfo("Error loading library: " + e.getMessage());
             throw new RuntimeException(e);
         }
+
+        System.out.println("Starting HTTP Server...");
+        StartGui.showInfo("Starting HTTP Server...");
+        try {
+            int port = 8080;
+            new HttpServer(port);
+            StartGui.showInfo(String.format("HTTP Server Started! [localhost:%s]", port));
+        } catch (IOException e) {
+            StartGui.showInfo("Error starting HTTP Server: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        StartGui.showInfo("Invoke library init...");
+        loaderNative = new JuiceLoaderNative();
+        loaderNative.init();
+        StartGui.showInfo("Library init success!");
+        StartGui.showInfo("Loaded Classes: " + loaderNative.getLoadedClasses().length);
+
+        StartGui.showInfo("JuiceTools is ready!");
+
+        StartGui.showControlPanel();
     }
 }
---
+++ src/main/java/cn/xiaozhou233/juicetools/gui/StartGui.java
@@ -0,0 +1,42 @@
+package cn.xiaozhou233.juicetools.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StartGui {
+    private static JFrame frame;
+    private static Bootstrap bootstrap = new Bootstrap();
+
+    public static void start() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            frame = new JFrame("JuiceTools Bootstrap");
+            frame.setContentPane(bootstrap.panel1);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setResizable(true);
+            frame.setVisible(true);
+        });
+    }
+
+    public static void showInfo(String info) {
+        bootstrap.info.append(info + "\n");
+    }
+
+    public static void showControlPanel() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame controlFrame = new JFrame("JuiceTools Control Panel");
+            ControlPanel controlPanel = new ControlPanel();
+            controlFrame.setContentPane(controlPanel.panel1);
+            controlFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            controlFrame.pack();
+            controlFrame.setResizable(true);
+            controlFrame.setVisible(true);
+        });
+    }
+}
